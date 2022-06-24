6bc6f98fa7b52cc7f9db399e4b1c592a4cdeb3ab
ARYAN SHARMA <96880954+aryansharma14@users.noreply.github.com>
2022-06-24 13:24:04 +0530

    added some problems

---
+++ basics/Basic_problems_/norm.js
@@ -0,0 +1,57 @@
+// JavaScript Program To Print Hello World
+// JavaScript Program to Add Two Numbers
+// JavaScript Program to Find the Square Root
+// JavaScript Program to Calculate the Area of a Triangle
+// JavaScript Program to Swap Two Variables
+// JavaScript Program to Convert Kilometers to Miles
+// Javascript Program to Convert Celsius to Fahrenheit
+// JavaScript Program To Work With Constants
+// JavaScript Program to Write to Console
+
+//Q1----------------------------------------------
+
+// console.log("hello world");
+
+// //or
+// alert("hello workld");
+
+//Q2----------------------------------------------
+// let a = parseInt(prompt("enter first number of ur choice "));
+// let b = parseInt(prompt("eneter second number of ur choice"));
+
+// const c = a + b;
+// console.log(c);
+
+//Q3-----------------------------------------------
+
+// let a = parseInt(prompt("enter the number"));
+// let x = Math.sqrt(a);
+// console.log(x);
+
+//Q4-------------------------------------------------
+
+// let height = parseInt(prompt("Enter the height for triangle"));
+// let breadth = parseInt(prompt("Enter the height for triangle"));
+// let area =  (height * breadth) / 2;
+// console.log(area);
+
+//Q5--------------------------------------------------------
+
+// let a = parseInt(prompt("enter the first variable"));
+// let b = parseInt(prompt("enter the second variable"));
+
+// console.log(a);
+// console.log(b);
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log(a);
+// console.log(b);
+
+//Q6-----------------------------------------------------------
+
+// let kilo = parseInt(prompt("enter the kilometers"));
+// let c = kilo * 0.62137;
+// console.log(c);
